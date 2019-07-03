0cc8e4661457d99b53693fabf80dd5c915374bdb
Panagiotis Christou <cnotis@gmail.com>
2019-07-04 00:50:36 +0300

    Applied deck and push no master

--- dwm-6.2/config.def.h
+++ dwm-6.2/config.def.h
@@ -53,6 +53,7 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
 #include "gaplessgrid.c"
+#include "fibonacci.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
@@ -61,6 +62,9 @@ static const Layout layouts[] = {
 	{ "|M|",      centeredmaster },
 	{ ">M>",      centeredfloatingmaster },
     { "###",	  gaplessgrid },
+ 	{ "[@]",      spiral },
+ 	{ "[\\]",     dwindle },
+	{ "[D]",      deck },
 };
 
 /* key definitions */
@@ -90,6 +94,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+    { MODKEY|ControlMask,           XK_j,      pushdown,       {0} },
+	{ MODKEY|ControlMask,           XK_k,      pushup,         {0} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
@@ -99,6 +105,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
     { MODKEY,                       XK_g,      setlayout,      {.v = &layouts[5]} },
+    { MODKEY,                       XK_s,      setlayout,      {.v = &layouts[6]} },
+    { MODKEY,                       XK_w,      setlayout,      {.v = &layouts[7]} },
+	{ MODKEY,                       XK_c,      setlayout,      {.v = &layouts[8]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
