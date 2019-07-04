d5c8cb297dc8fd8e3f3bcd61460d4d4e0cc435f8
Panagiotis Christou <cnotis@gmail.com>
2019-07-04 18:14:22 +0300

    Added shallow patch

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -31,20 +31,21 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       1 << 6,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "Thunderbird",  NULL,       NULL,   1 << 1,       0,           -1 },
-	{ "St",       NULL,       NULL,       1,       0,           -1 },
-	{ NULL,  "Viber",       NULL,       1 << 3,       0,           -1 },
-	{ "Slack",  NULL,       NULL,       1 << 3,       0,           -1 },
-	{ "Mpv",  NULL,       NULL,       1 << 4,       0,           -1 },
-	{ "Vlc",  NULL,       NULL,       1 << 4,       0,           -1 },
-	{ "Subl3",  NULL,       NULL,       1 << 5,       0,           -1 },
-	{ "Gvim",  NULL,       NULL,       1 << 5,       0,           -1 },
-	{ "Zathura",  NULL,       NULL,       1 << 6,       0,           -1 },
-	{ "Sxiv",  NULL,       NULL,       1 << 6,       0,           -1 },
-	{ "DesktopEditors",  NULL,       NULL,       1 << 6,       0,           -1 },
+	/* class      instance    title       tags mask     isfloating   isterminal noswallow monitor */
+	{ "Gimp",     NULL,       NULL,       1 << 6,            1,     0, 0,      -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 1,       0,      0, 0,     -1 },
+	{ "Thunderbird",  NULL,       NULL,   1 << 1,       0,      0, 0,     -1 },
+	{ "St",       NULL,       NULL,       1,       0,         1, 1,  -1 },
+	{ NULL,  "Viber",       NULL,       1 << 3,       0,      0, 0,     -1 },
+	{ "Slack",  NULL,       NULL,       1 << 3,       0,      0, 0,     -1 },
+	{ "Mpv",  NULL,       NULL,       1 << 4,       0,        0, 0,   -1 },
+	{ "Vlc",  NULL,       NULL,       1 << 4,       0,        0, 0,   -1 },
+	{ "Subl3",  NULL,       NULL,       1 << 5,       0,      0, 0,     -1 },
+	{ "Gvim",  NULL,       NULL,       1 << 5,       0,       0, 0,    -1 },
+	{ "Zathura",  NULL,       NULL,       1 << 6,       0,    0, 0,       -1 },
+	{ "Sxiv",  NULL,       NULL,       1 << 6,       0,       0, 0,     -1 },
+	{ "DesktopEditors",  NULL,       NULL,       1 << 6,       0, 0,0,           -1 },
+
 };
 
 /* layout(s) */
