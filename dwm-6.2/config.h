c574ceecf041a68e4bb68d89b2be7eb84a94a275
Panagiotis Christou <cnotis@gmail.com>
2019-07-06 21:42:23 +0300

    Added icons in tags

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -10,13 +10,13 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "xos4 Terminus:size=9", "TerminessTTF Nerd Font:size=9" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#000000";
-static const char col_cyan[]        = "#79740e";
+static const char col_cyan[]        = "#076678";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -24,7 +24,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "$" , "W", "F", "@", "#", "{}", "D", "S", "O" };
+static const char *tags[] = { "" , "" , "", "", "", "", " ", " ", " " };
 
 static const Rule rules[] = {
 	/* xprop(1):
