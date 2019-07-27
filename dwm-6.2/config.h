01fec7942d39cfbdaa6cce8f5216fa196fa21b14
Panagiotis Christou <cnotis@gmail.com>
2019-07-28 01:36:09 +0300

    Applied tab patch

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -10,6 +10,14 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+
+/*   Display modes of the tab bar: never shown, always shown, shown only in */
+/*   monocle mode in presence of several windows.                           */
+/*   Modes after showtab_nmodes are disabled                                */
+enum showtab_modes { showtab_never, showtab_auto, showtab_nmodes, showtab_always};
+static const int showtab            = showtab_auto; /* Default tab bar show mode */
+static const int toptab             = True;    /* False means bottom tab bar */
+
 static const char *fonts[]          = { "xos4 Terminus:size=9", "TerminessTTF Nerd Font:size=9" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
@@ -96,6 +104,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_w,      tabmode,        {-1} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
@@ -114,7 +123,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
     { MODKEY,                       XK_g,      setlayout,      {.v = &layouts[5]} },
     { MODKEY,                       XK_s,      setlayout,      {.v = &layouts[6]} },
-    { MODKEY,                       XK_w,      setlayout,      {.v = &layouts[7]} },
+    { MODKEY,                       XK_q,      setlayout,      {.v = &layouts[7]} },
 	{ MODKEY,                       XK_c,      setlayout,      {.v = &layouts[8]} },
     { MODKEY,                       XK_Escape,           toggleAttachBelow,           {0} },
     /*{ MODKEY|ControlMask,		    XK_comma,  cyclelayout,    {.i = -1 } },*/
@@ -155,5 +164,6 @@ static Button buttons[] = {
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+	{ ClkTabBar,            0,              Button1,        focuswin,       {0} },
 };
 
